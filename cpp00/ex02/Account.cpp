d1e895a2796e707e259abfbe5d31b4cfbcd1f09a
Дарья Сидельникова <darasidelnikova@Daras-MacBook-Pro.local>
2024-01-07 15:30:08 +0100

    hihih

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -22,7 +22,7 @@ int	Account::_totalNbWithdrawals;
 
 void	Account::_displayTimestamp( void )
 {
-	auto currentTime = std::chrono::system_clock::now();
+	std::chrono::system_clock::time_point currentTime = std::chrono::system_clock::now();
 
     std::time_t currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
 
@@ -37,18 +37,20 @@ Account::Account( int initial_deposit )
 	_nbAccounts++;
 	_accountIndex = _nbAccounts - 1;
 	_totalAmount += initial_deposit;
-	std::cout << "time: " << _displayTimestamp
-			<< "account #" << _accountIndex << "created"
-			<< "\namount: " << initial_deposit << std::endl;
+	std::cout << "time: " << std::flush;
+	_displayTimestamp();
+	std::cout << "account  #" << _accountIndex << "  created"
+			<< "\namount: " << initial_deposit << "\n\n" << std::endl;
 }
 
 Account::~Account( void )
 {
 	_nbAccounts--;
 	_totalAmount -= _amount;
-	std::cout << "time: " << _displayTimestamp
-			<< "account #" << _accountIndex << "closed"
-			<< "\namount: " << _amount << std::endl;
+	std::cout << "time: " << std::flush;
+	_displayTimestamp();
+	std::cout << "account  #" << _accountIndex << "  closed"
+			<< "\namount: " << _amount  << "\n\n" << std::endl;
 }
 
 int	Account::getNbAccounts( void )
@@ -73,17 +75,58 @@ int	Account::getNbWithdrawals( void )
 
 void	Account::displayAccountsInfos( void )
 {
+	std::cout << "time: " << std::flush;
 	_displayTimestamp();
 	std::cout << "accounts: " << getNbAccounts()
 			<< "\ntotal amount: " << getTotalAmount()
 			<< "\ndeposits: " << getNbDeposits()
-			<< "\nwithdrawals: " << getNbWithdrawals() << std::endl;
+			<< "\nwithdrawals: " << getNbWithdrawals() << "\n\n" << std::endl;
 }
 	
-	void	makeDeposit( int deposit );
-	bool	makeWithdrawal( int withdrawal );
-	int		checkAmount( void ) const;
-	void	displayStatus( void ) const;
+void	Account::makeDeposit( int deposit )
+{
+	std::cout << "time: " << std::flush;
+	_displayTimestamp();
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
+	std::cout << "account  # " << _accountIndex << "\namount of deposits: "
+			<< _nbDeposits - 1 << "\ncurrent amount: "
+			<< _amount << "\ndeposit: " << deposit << std::endl;
+	_amount += deposit;
+	std::cout << "updated amount: " << _amount << "\n\n" << std::endl;
+}
+
+int		Account::checkAmount( void ) const
+{
+	return _amount;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	_displayTimestamp();
+	std::cout << "account  # " << _accountIndex
+			<< "\ncurrent amount: " << _amount
+			<< "\namount to withdraw: " << withdrawal << std::endl;
+	if (withdrawal > checkAmount())
+	{
+		std::cout << "withdrawal refused, not enough balance for transaction"  << "\n\n" << std::endl;
+		return false;
+	}
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_amount -= withdrawal;
+	std::cout << "withdrawal successesful\n" << "balance: " << _amount << "\n\n" << std::endl;
+	return true;
+}
+
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "account  #" << _accountIndex << "\namount: " << _amount << "\ndeposits: "
+			<< _nbDeposits << "\nwithdrawals: " << _nbWithdrawals << "\n\n" << std::endl;
+}
 
 
 
